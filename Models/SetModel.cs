a4f383dbd0b64660d89b1333baebd53784e73398
jboundy <j.a.boundy@gmail.com>
2016-06-12 17:48:57 -0500

    Removed old code to add new library for api calls

---
+++ Models/SetModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Magic_PoC.DAL.Models
+{
+    public class SetModel
+    {
+        [JsonProperty(PropertyName = "code")]
+        public string Code { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+        [JsonProperty(PropertyName = "border")]
+        public string Border { get; set; }
+        [JsonProperty(PropertyName = "mkm_id")]
+        public string MkmId { get; set; }
+        [JsonProperty(PropertyName = "mkm_name")]
+        public string MkmName { get; set; }
+        [JsonProperty(PropertyName = "releaseDate")]
+        public string ReleaseDate { get; set; }
+        [JsonProperty(PropertyName = "magicCardsInfoCode")]
+        public string MagicCardsInfoCode { get; set; }
+        [JsonProperty(PropertyName = "block")]
+        public string Block { get; set; }
+    }
+}
---
+++ SetsRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Magic_PoC.DAL
+{
+    public class SetsRepository
+    {
+        private HttpClient _client;
+        public SetsRepository()
+        {
+            using(var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.magicthegathering.io/v1/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                _client = client;
+            }
+        }
+
+        public async Task GetSetCards(string set)
+        {
+            HttpResponseMessage response = await _client.GetAsync("sets");
+            if (response.IsSuccessStatusCode)
+            {
+                var cards = await response.Content.ReadAsAsync<Product>();
+                Console.WriteLine("{0}\t${1}\t{2}", product.Name, product.Price, product.Category);
+            }
+
+
+
+        }
+    }
+}
